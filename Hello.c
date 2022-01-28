bcc70dac66d8bb10ee24ada7c728435051153db3
gnichter <gnichter2@huskers.unl.edu>
2022-01-28 15:33:06 +0000

    added my major to the output

---
+++ Hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Grant Nichter 
+ *         gnichter2@huskers.unl.edu
+ * Date: 2020/01/28
+ *
+ * this program prints out my Name and Major
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Grant Nichter!!!\n");
+  printf("Computer Engineering :D\n");
+
+  return 0;
+}
